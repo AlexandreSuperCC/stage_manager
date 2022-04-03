3733a5b6fd571c93dfd8a7662e9fd8a70a526b27
AlexandreSuperCC <yuancaocc@163.com>
2022-04-03 21:35:56 +0200

    first commit

--- stagefile/Vyte/vyte-dev-challenge-master_code_Yuan_Cao/appointmentUtil.js
+++
@@ -1,9 +0,0 @@
-function getEmailIndex(arr, email){
-    for(var i=0;i<arr.length;i++){
-        if(arr[i].email == email){
-            return i;
-        }
-    }
-    return -1;
-}
-module.exports = getEmailIndex;
